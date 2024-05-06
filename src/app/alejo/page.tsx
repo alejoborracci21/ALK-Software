f1dc022653e27cb1fe0c1ccc17cc8af8759c7992
Alejo Borracci <borraccialejo@hotmail.es>
2024-05-06 13:25:07 -0300

    first commit

---
+++ src/app/alejo/page.tsx
@@ -0,0 +1,8 @@
+export default function Alejo() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Alejo page</h1>
+      </main>
+    );
+  }
+  
