d63c4f0d22d3e328637b6be74712f29dbb5efcf8
Alejo Borracci <borraccialejo@hotmail.es>
2024-05-08 21:10:29 -0300

    send email

--- src/app/contact/components/form.tsx
+++ src/app/contact/components/form.tsx
@@ -1,83 +1,88 @@
 'use client'
 
-import styles from "../page.module.css"
-import { useState } from "react"
+
+import styles from "../page.module.css";
+import { useState } from "react";
+import { sendEmail } from "../hooks/email.js";
 
 interface Mensaje {
-  name: string,
-  subname: string,
-  email: string,
-  msj: string
+  user_name: string;
+  user_email: string;
+  message: string;
 }
 
-const initialState = {
-    name: "",
-    subname: "",
-    email:"",
-    msj: ""
-}
-
+const initialState: Mensaje = {
+  user_name: "",
+  user_email: "",
+  message: ""
+};
 
 export const Form = () => {
+  const [mensaje, setMensaje] = useState<Mensaje>(initialState);
 
+  const onSubmit = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
 
-    const [mensaje, setMensaje] = useState<Mensaje>(initialState)
-    
-    const onSubmit = (evento:any) => {
-      evento.preventDefault(); // Esto evita que la página se recargue al enviar el formulario
+    const formElement = evento.currentTarget;
+    sendEmail(formElement);
 
-      const formData = new FormData(evento.target); // Crea un objeto FormData con los datos del formulario
-      const formValues: any = {};
+    setMensaje(initialState);
+    alert(
+      "Su mensaje fue enviado correctamente. Te responderemos en menos de 24 horas!"
+    );
+  };
 
-      // Itera sobre los campos del formulario y guarda sus valores en formValues
-      formData.forEach((value, key) => {
-        formValues[key] = value;
-      });
+  const onChange = (
+    evento: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setMensaje({
+      ...mensaje,
+      [evento.target.name]: evento.target.value
+    });
+  };
 
-      // formValues ahora contiene un objeto con los valores de todos los campos del formulario
-      console.log(formValues);
+  return (
+    <div className={styles.container}>
+      <form onSubmit={onSubmit} className={styles.form}>
+        <label className={styles.label}>
+          Nombre:
+          <input
+            name="user_name"
+            onChange={onChange}
+            className={styles.input}
+            type="text"
+            value={mensaje.user_name}
+            placeholder="Nombre completo"
+          />
+        </label>
 
-      // También puedes enviar formValues a través de una solicitud HTTP aquí
+        <label className={styles.label}>
+          Email:
+          <input
+            name="user_email"
+            onChange={onChange}
+            className={styles.input}
+            type="email"
+            value={mensaje.user_email}
+            placeholder="Tu Email"
+          />
+        </label>
 
-      setMensaje(initialState); // Reinicia el estado del formulario
-      alert(
-        "Su mensaje fue enviado correctamente. Te responderemos en menos de 24 horas!"
-      );
-    }
-    
-      const onChange = (evento:React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-        setMensaje({
-          ...mensaje,
-          [evento.target.name]: evento.target.value
-        })
-      }
+        <label className={styles.label}>
+          Dejanos tu consulta:
+          <textarea
+            name="message"
+            onChange={onChange}
+            className={styles.input}
+            value={mensaje.message}
+            placeholder="Deje aqui su mensaje"
+          ></textarea>
+        </label>
 
-
-    return(
-        <div className={styles.container}>
-        <form onSubmit={onSubmit}className={styles.form}>
-          <label className={styles.label}>
-             Nombre:
-            <input  name="name" onChange={onChange} className={styles.input} type="text" value={mensaje.name} placeholder="Nombre"></input>
-          </label>
-
-          <label className={styles.label}>
-             Apellido:
-            <input  name="subname" onChange={onChange} className={styles.input} type="text" value={mensaje.subname} placeholder="Apellido"></input>
-          </label>
-
-          <label className={styles.label}>
-             Email:
-            <input  name="email" onChange={onChange} className={styles.input} type="email" value={mensaje.email} placeholder="Tu Email"></input>
-          </label>
-
-          <label className={styles.label}>
-             Dejanos tu consulta:
-            <textarea name="msj" onChange={onChange} className={styles.input} value={mensaje.msj} placeholder="Deje aqui su mensaje"></textarea>
-          </label>
-
-          <button className={styles.button} type="submit">Enviar</button>
-        </form>
-      </div>
-    )
-}
+        <button className={styles.button} type="submit">
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+};
