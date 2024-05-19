1d89ac52df1ea33cdb02eca46ed50277ae66ad16
Alejo Borracci <borraccialejo@hotmail.es>
2024-05-19 03:03:14 -0300

    proyectos

--- src/app/portfolio/components/list.tsx
+++ src/app/portfolio/components/list.tsx
@@ -1,28 +1,29 @@
-import styles from "./list.module.css"
-import proyectos from "../proyects.json"
+import styles from "./list.module.css";
+import proyectos from "../proyects.json";
+import wearfashion from "./wearfashion.jpg";
 
 interface Proyecto {
-  name: string,
-  description: string,
-  image: string
+  name: string;
+  description: string;
+  image: string;
 }
 
-const {proyects} = proyectos
+const { proyects } = proyectos;
 
 
 
 export const ListProyect = () => {
-
-
-    return (
-      <div className={styles.container}>
-        <div>LISTA DE PROYECTOS</div>
-        <div>{proyects.map((p:any) => 
-        <div className={styles.proyect}>
-            <h1 className={styles.name}>{p.name}</h1>
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>LISTA DE PROYECTOS</div>
+      {proyects.map((p: Proyecto, index: number) => (
+        <div key={index} className={styles.proyect}>
+          <h1 className={styles.name}>{p.name}</h1>
+          <div className={styles.text}>
             <p className={styles.description}>{p.description}</p>
-            <h1 className={styles.image}>{p.image}</h1>
-        </div>)}</div>
-      </div>
-    );
-}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
