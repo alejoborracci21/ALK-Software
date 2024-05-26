48750750b31c8930f99753ed2c9f24ac08965adc
Alejo Borracci <borraccialejo@hotmail.es>
2024-05-26 20:50:06 -0300

    estructura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Form } from "./contact/components/form";
+import { Form } from "./pages/contact/components/form";
 import styles from "./pagestyles.module.css"
 import Contratanos from './components/contratanos/Contratanos'
 export default function Landing() {
